0ad7f464cd045cf6c2cbf865de177383860a9d20
Christian Ascone <ascone.christian@gmail.com>
2016-08-09 14:49:57 +0200

    Refactoring

--- src/angular-heatmap.js
+++ src/angular-heatmap.js
@@ -1,53 +1,36 @@
 angular.module('angular-heatmap-js', []);
 
 angular.module('angular-heatmap-js')
-.controller('AngularHeatmapCtrl', function ($scope) {
+  .controller('angularHeatmapController', function($scope) {
+    var ctrl = this;
 
-           // now generate some random data
-            var points = [];
-            var max = 0;
+    ctrl.init = function() {
+      var div = document.getElementById("angular-heatmap-js-container");
 
-            var div = document.getElementById("angular-heatmap-js-container");
-            var width = div.style.width;
-            var height = div.style.height;
-            var len = 200;
+      // Set container style
+      var width = "100%";
+      var height = "100%";
+      div.style.width = width;
+      div.style.height = height;
 
-            while (len--) {
-                var val = Math.floor(Math.random()*100);
-                max = Math.max(max, val);
-                var point = {
-                    x: Math.floor(Math.random()*width),
-                    y: Math.floor(Math.random()*height),
-                    value: val
-                };
-                points.push(point);
-            }
-            // heatmap data format
-            $scope.passed_data = { 
-                max: max, 
-                data: points 
-            };
-    })
-    .directive('angularHeatmap', function(){
-        return {
-            restrict: 'E',
-            scope: {
-                data: '='
-            },
-            template: '<div id="angular-heatmap-js-container" container></div>',
-            link: function(scope, ele, attr){
-                var div = document.getElementById("angular-heatmap-js-container");
+      // New instance for heatmap with div as container
+      ctrl.heatmapInstance = h337.create({
+        container: div
+      });
+      // Using bind data for heatmap init
+      ctrl.heatmapInstance.setData(ctrl.heatmapData);
+    }
+    ctrl.init();
 
-                var width = "400px";
-                var height = "200px";
-
-                div.style.width = width;
-                div.style.height = height;
-                scope.heatmapInstance = h337.create({
-                  container: ele.find('div')[0]
-                });
-                scope.heatmapInstance.setData(scope.data);
-            }
-
-        };
-    });
+  })
+  .directive('angularHeatmap', function() {
+    return {
+      restrict: 'E',
+      bindToController: {
+        'heatmapData': '='
+      },
+      template: '<div id="angular-heatmap-js-container" container></div>',
+      controller: 'angularHeatmapController',
+      controllerAs: 'angularHeatmapCtrl'
+    };
+  });
