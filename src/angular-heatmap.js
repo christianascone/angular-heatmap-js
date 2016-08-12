2e9f3dee9082191af7e34f4a4f26a2f9a9a30f44
christianascone <ascone.christian@gmail.com>
2016-08-12 18:34:17 +0200

    Tooltip configuration

--- src/angular-heatmap.js
+++ src/angular-heatmap.js
@@ -3,32 +3,71 @@ angular.module('angular-heatmap-js', []);
 angular.module('angular-heatmap-js')
   .controller('angularHeatmapController', function($scope) {
     var ctrl = this;
+    ctrl.container = {};
 
     ctrl.init = function() {
-      var div = document.getElementById("angular-heatmap-js-container");
+      ctrl.container = document.getElementById("angular-heatmap-js-container");
 
       // Set container style
       var width = "100%";
       var height = "100%";
-      div.style.width = width;
-      div.style.height = height;
+      ctrl.container.style.width = width;
+      ctrl.container.style.height = height;
 
-      if(ctrl.width){
-        div.style.width = ctrl.width+"px";
+      // Set specific width and height if available
+      if (ctrl.width) {
+        ctrl.container.style.width = ctrl.width + "px";
+      }
+      if (ctrl.height) {
+        ctrl.container.style.height = ctrl.height + "px";
       }
 
-      if(ctrl.height){
-        div.style.height = ctrl.height+"px";
-      }
-
-      // New instance for heatmap with div as container
+      // New instance for heatmap with ctrl.container as container
       ctrl.heatmapInstance = h337.create({
-        container: div
+        container: ctrl.container
       });
-      // Using bind data for heatmap init
+      // Using data binding for heatmap init
       ctrl.heatmapInstance.setData(ctrl.heatmapData);
+
+      if (ctrl.tooltipEnabled) {
+        ctrl.initTooltip();
+      }
+
     }
-    ctrl.init();
+
+    ctrl.initTooltip = function() {
+      ctrl.tooltip = document.querySelector('#heatmap-tooltip');
+
+      // When the mouse is hover the heatmap, it checks
+      // the coordinates and set the tooltip visibility
+      ctrl.container.onmousemove = function(ev) {
+        var x = ev.layerX;
+        var y = ev.layerY;
+        // getValueAt gives us the value for a point p(x/y)
+        var value = ctrl.heatmapInstance.getValueAt({
+          x: x,
+          y: y
+        });
+
+        ctrl.tooltip.style.display = 'block';
+        ctrl.tooltip.style.opacity = '.9';
+
+        updateTooltip(x, y, value);
+      };
+
+      // hide ctrl.tooltip on mouseout
+      ctrl.container.onmouseout = function() {
+        ctrl.tooltip.style.display = 'none';
+      };
+    }
+
+
+    function updateTooltip(x, y, value) {
+      // + 15 for distance to cursor
+      var transl = 'translate(' + (x + 15) + 'px, ' + (y + 15) + 'px)';
+      ctrl.tooltip.style.webkitTransform = transl;
+      ctrl.tooltip.innerHTML = value;
+    }
 
   })
   .directive('angularHeatmap', function() {
@@ -39,7 +78,13 @@ angular.module('angular-heatmap-js')
         'width': '=',
         'height': '='
       },
-      template: '<div id="angular-heatmap-js-container" container></div>',
+      link: function(scope, element, attrs, ctrl) {
+        if (angular.isDefined(attrs.heatmapTooltip)) {
+          ctrl.tooltipEnabled = true;
+        }
+        ctrl.init();
+      },
+      template: '<div id="heatmap-tooltip" class="tooltip"></div>' + '<div id="angular-heatmap-js-container" container></div>',
       controller: 'angularHeatmapController',
       controllerAs: 'angularHeatmapCtrl'
     };
